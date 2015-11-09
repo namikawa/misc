8585224342d185a39222a58ef3171681ea1206d7
namikawa <rx7.fd3s.rs@gmail.com>
2015-11-09 17:25:32 +0900

    initial commit

---
+++ scripts/pg_backup.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+### config
+SSH_USER="postgres"
+PG_USER="postgres"
+
+BACKUP_BASE_DIR="/backup"
+GCS_BACKET_NAME="namikawa-test"
+SPLIT_FILE_SIZE="1G"
+
+# download speed of pg_dump (KB/s)
+D_SPEED="5000"
+# upload speed to Google Cloud Storage (KB/s)
+U_SPEED="1000"
+
+### require
+# - Google Cloud SDK (gsutil)
+# - Bandwidth shaper tool (trickle)
+# - Split a file (split)
+hash gsutil && hash trickle && hash split
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: require tools are not installed." 1>&2
+  exit 1
+fi
+
+### arg & check
+usage() {
+  echo "Usage: $ ${0} -h [HOST_ADDDRESS] -d [DATABASE_NAME] -g [GENERATION]"
+  exit 1
+}
+
+while getopts ':d:g:h:' OPT
+do
+  case ${OPT} in
+    d)  DATABASE=${OPTARG}
+        ;;
+    g)  GENERATION=${OPTARG}
+        ;;
+    h)  HOST_ADDRESS=${OPTARG}
+        ;;
+    :|\?) usage
+        ;;
+  esac
+done
+shift $((${OPTIND} - 1))
+
+([ -z "${DATABASE}" ] || [ -z "${GENERATION}" ] || [ -z "${HOST_ADDRESS}" ]) && usage
+
+### exec
+exit_check() {
+  if [ $? -ne 0 ]; then
+    echo "[`date +"%Y/%m/%d %k:%M:%S"`] ERROR: abort." 1>&2
+    exit 1
+  fi
+}
+
+# make backup directory
+DATE=`date +"%Y%m%d_%k%M%S"`
+BACKUP_DIR="${BACKUP_BASE_DIR}/${DATABASE}/${DATE}"
+DUMP_FILE="${BACKUP_DIR}/${DATE}_${DATABASE}.dump"
+
+if [ -e ${BACKUP_DIR} ]; then
+  echo "ERROR: ${BACKUP_DIR} already exists." 1>&2
+  exit 1
+fi
+
+mkdir -p ${BACKUP_DIR}
+
+# backup database
+echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: start pg_dump."
+trickle -s -d ${D_SPEED} ssh ${SSH_USER}@${HOST_ADDRESS} "pg_dump -Fc -h 127.0.0.1 -U ${PG_USER} ${DATABASE}" > ${DUMP_FILE}
+exit_check
+echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: finished pg_dump."
+
+# split dump file
+split -b ${SPLIT_FILE_SIZE} -a 4 -d ${DUMP_FILE} ${DUMP_FILE}.
+exit_check
+rm -f ${DUMP_FILE}
+
+# upload backup file
+echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: start uploading."
+trickle -s -u ${U_SPEED} gsutil -m rsync -r  ${BACKUP_DIR} gs://${GCS_BACKET_NAME}/${DATABASE}/${DATE}/
+exit_check
+echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: finished uploading."
+
+# delete backup directory
+rm -rf ${BACKUP_DIR}
+
+# manage backup generation
+BACKUP_PATHS=`gsutil ls gs://${GCS_BACKET_NAME}/${DATABASE}/ | sort -k5 -r`
+COUNT=1
+
+for BACKUP_PATH in ${BACKUP_PATHS}; do
+  if [ ${COUNT} -le ${GENERATION} ]; then
+    # N/A
+    echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: keeping - ${BACKUP_PATH}"
+  else
+    # delete
+    gsutil -m rm -r ${BACKUP_PATH}
+    exit_check
+    echo "[`date +"%Y/%m/%d %k:%M:%S"`] INFO: deleting - ${BACKUP_PATH}"
+  fi
+  COUNT=`expr ${COUNT} + 1`
+done
+
