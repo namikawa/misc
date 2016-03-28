2cf272966845bb784e48436ef8a25815da013d6a
namikawa <rx7.fd3s.rs@gmail.com>
2016-03-28 12:22:50 +0900

    initial commit

---
+++ scripts/gcp/exec_snapshot_all_disks.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+GCLOUD = "gcloud"
+ZONE = "asia-east1-a"
+
+project_opt = ""
+unless ARGV[0].nil? then
+  project_opt = "--project=#{ARGV[0]}"
+else
+  STDERR.puts "[ERROR] invalid arguments. (ex. $ #{File.basename(__FILE__)} [Project Name])"
+  exit!(1)
+end
+
+list_res = JSON.parse(`#{GCLOUD} compute disks list #{project_opt} --format=json`)
+
+disks = ""
+snapshots = ""
+time = Time.now.strftime("%Y%m%d-%H%M%S")
+
+list_res.each do |disk|
+  if disks.empty? and snapshots.empty? then
+    disks = disk['name']
+    snapshots = disk['name'] + "-" + time
+  else
+    disks += " " + disk['name']
+    snapshots += "," + disk['name'] + "-" + time
+  end
+end
+puts "--- Target Disks   : " + disks
+puts "--- Snapshot Names : " + snapshots
+
+snap_res = `#{GCLOUD} compute disks snapshot #{disks} --snapshot-names #{snapshots} --zone #{ZONE} #{project_opt}`
+puts snap_res
+
