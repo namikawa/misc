1b83f22919a47a23047882ec7315daf20fd9a053
namikawa <rx7.fd3s.rs@gmail.com>
2016-04-01 18:31:53 +0900

    fix

--- scripts/lotate_backup_link.sh
+++ scripts/lotate_backup_link.sh
@@ -4,7 +4,7 @@ set -e
 BACKUP_DIR="/data/backup"
 FILTER="[0-9]\{4\}[0-9]\{2\}[0-9]\{2\}_[0-9]\{2\}[0-9]\{2\}[0-9]\{2\}"
 
-link_list=(`find /data/backup/qnap -maxdepth 1 -type l | grep -e "${FILTER}" | sort`)
+link_list=(`find ${BACKUP_DIR} -maxdepth 1 -type l | grep -e "${FILTER}" | sort`)
 target_path=`readlink ${link_list[0]}`
 
 rm ${link_list[0]}
