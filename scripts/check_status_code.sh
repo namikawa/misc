84de29adba5042a0eab20bb693a697918e2434e8
namikawa <rx7.fd3s.rs@gmail.com>
2016-06-24 23:03:25 +0900

    fix

--- scripts/check_status_code.sh
+++ scripts/check_status_code.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 DOMAINS=(
+twitter.com
+apple.com
+google.com
 )
 
 for domain in ${DOMAINS[@]}; do
-  result=`curl -sI -o /dev/null -w '%{http_code}\n' ${domain}`
-  #result=`curl -sIL -o /dev/null -w '%{http_code}\n' ${domain}`
+  #result=`curl -sI -o /dev/null -w '%{http_code}\n' ${domain}`
+  result=`curl -sIL -o /dev/null -w '%{http_code}\n' ${domain}`
   echo "${domain} : ${result}"
 done
 
