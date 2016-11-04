ebd22cdabfa1d3a7a5f1f01bd4ad571e5cfb2bae
namikawa <rx7.fd3s.rs@gmail.com>
2016-11-04 17:47:07 +0900

    fix

--- scripts/package_update.sh
+++ scripts/package_update.sh
@@ -10,15 +10,13 @@ brew update
 echo "------------------------------ brew upgrade ------------------------------"
 brew upgrade
 
-#echo "------------------------------ easy_install -U pip ------------------------------"
-#sudo easy_install -U pip
 echo "------------------------------ pip install --upgrade pip ------------------------------"
-sudo -H pip install --upgrade pip
+pip install --upgrade pip
 
-echo "------------------------------ pip-review --auto  ------------------------------"
-sudo -H pip-review --auto
-#echo "------------------------------ pip install -U *** ------------------------------"
-#sudo -H pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | egrep -v "pyobjc-framework-" | sudo -H xargs pip install -U
+#echo "------------------------------ pip-review --auto  ------------------------------"
+#sudo -H pip-review --auto
+echo "------------------------------ pip install -U *** ------------------------------"
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | sudo -H xargs pip install -U
 
 echo "------------------------------ gem update  ------------------------------"
 sudo gem update
