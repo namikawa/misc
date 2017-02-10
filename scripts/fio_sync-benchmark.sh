6ab6b8348c3c6ba637bf9457a5c0733978c4ab05
namikawa <rx7.fd3s.rs@gmail.com>
2017-02-10 17:39:58 +0900

    add patterns for throughput

--- scripts/fio_sync-benchmark.sh
+++ scripts/fio_sync-benchmark.sh
@@ -7,6 +7,7 @@ RUNTIME=60
 SLEEPTIME=10
 
 # exec
+echo -e "===== IOPS =====\n"
 for NUMJOB in 1 2 4 8 16 32 64 128 256; do
   echo "----- numjobs=$NUMJOB -----"
   for TYPE in read write randread randwrite; do
@@ -14,8 +15,17 @@ for NUMJOB in 1 2 4 8 16 32 64 128 256; do
     fio -direct=1 -readwrite=$TYPE -group_reporting \
         -filename=$FILENAME -size=$SIZE -runtime=$RUNTIME \
         -bs=4k -numjobs=$NUMJOB -name=file1 | grep "iops="
-    echo 3 > /proc/sys/vm/drop_caches
+    #echo 3 > /proc/sys/vm/drop_caches
     sleep $SLEEPTIME
   done
+done
+
+echo -e "\n===== Throughput =====\n"
+for TYPE in read write; do
+  fio -direct=1 -readwrite=$TYPE -group_reporting \
+      -filename=$FILENAME -size=$SIZE -runtime=$RUNTIME \
+      -bs=32m -numjobs=16 -name=file1 | egrep "READ:|WRITE:"
+  echo 3 > /proc/sys/vm/drop_caches
+  sleep $SLEEPTIME
 done
 
