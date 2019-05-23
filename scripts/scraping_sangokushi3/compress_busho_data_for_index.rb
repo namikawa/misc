7143b1cda7de5041f250744a7cee1bba71b80173
namikawa <rx7.fd3s.rs@gmail.com>
2019-05-24 00:53:55 +0900

    add id number

--- scripts/scraping_sangokushi3/compress_busho_data_for_index.rb
+++ scripts/scraping_sangokushi3/compress_busho_data_for_index.rb
@@ -1,6 +1,9 @@
 require 'csv'
 
+id = 1
+
 CSV.foreach(ARGV[0], headers: true) do |row|
-  puts "#{row['武力']},#{row['知力']},#{row['政治']},#{row['魅力']},#{row['陸指']},#{row['水指']},\"#{row['名前']}\""
+  puts "#{row['武力']},#{row['知力']},#{row['政治']},#{row['魅力']},#{row['陸指']},#{row['水指']},\"#{row['名前']}\",#{id}"
+  id += 1
 end
 
