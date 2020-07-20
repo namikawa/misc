445ecf95ad504934eaf814b56e8273eaf3c18d74
namikawa <rx7.fd3s.rs@gmail.com>
2020-07-21 02:36:50 +0900

    initial commit.

---
+++ scripts/tcpping.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Usage:
+# ./tcpping.sh {HOST} {PORT}
+
+INTERVAL=1
+##
+
+seq=1
+
+while true
+do
+  res=`(time nc -vz -G ${INTERVAL} $@) 2>&1 | egrep ' port |real '`
+  res=`echo ${res} | sed -e "s/[\r\n]\+//g"`
+
+  if [ "`echo ${res} | grep 'Operation timed out'`" ] ;
+  then
+    : # N/A
+  else
+    sleep ${INTERVAL}
+  fi
+
+  echo "seq[${seq}]: ${res}"
+  seq=$((seq+1))
+done
+
